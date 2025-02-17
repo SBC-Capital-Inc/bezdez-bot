554af9e7039c9cf7d644765b86cd18cdb30b8c31
Alexander Suvorov <alx.suvorov@gmail.com>
2025-02-17 14:21:17 +0100

    suvorov @ macbook-pro

--- suv-container-stop.sh
+++ suv-container-stop.sh
@@ -6,7 +6,7 @@ source "suv-options.sh"
 set -eu
 
 echo
-echo -e ${ANSI_BG_BLUE}"Stopping container" ${ANSI_BG_PURPLE}${CONTAINER_NAME}${ANSI_OFF} ${ANSI_BG_BLUE}"..."${ANSI_OFF}
+echo -e ${ANSI_BG_BLUE}"Stopping container"${ANSI_OFF} ${ANSI_BG_PURPLE}${CONTAINER_NAME}${ANSI_OFF} ${ANSI_BG_BLUE}"..."${ANSI_OFF}
 docker compose -f ${CONTAINER_YAML} down
 
 echo
