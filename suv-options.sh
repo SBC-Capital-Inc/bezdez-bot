36ec2a8b4db66853ddc908e0c25eccbb717f5ec9
Alexander Suvorov <alx.suvorov@gmail.com>
2025-02-17 12:50:29 +0100

    suvorov @ macbook-pro

--- suv-options.sh
+++ suv-options.sh
@@ -4,14 +4,17 @@ bot2_error_log="bot2-error.log"
 # 
 # https://stackoverflow.com/questions/75608323/how-do-i-solve-error-externally-managed-environment-every-time-i-use-pip-3
 # 
-venv_name="venv"
-venv_path="venv"
-venv_activate="$venv_path/bin/activate"
-venv_pip="$venv_path/bin/pip"
-venv_python3="$venv_path/bin/python3"
+VENV_NAME="venv"
+VENV_PATH="venv"
+VENV_ACTIVATE="$VENV_PATH/bin/activate"
+VENV_PIP="$VENV_PATH/bin/pip"
+VENV_PYTHON3="$VENV_PATH/bin/python3"
 
 bot2_py="bot2.py"
 
+#
+# Это нужно для скриптов git
+#
 HOSTNAME_LOCAL="macbook-pro"
 
 GIT_REPOSITORY="SBC-Capital-Inc/bezdez-bot.git"
