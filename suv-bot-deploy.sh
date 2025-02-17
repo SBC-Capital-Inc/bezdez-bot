3de92e5298bf41f46a624cc567fc34d77bcd2390
Alexander Suvorov <alx.suvorov@gmail.com>
2025-02-17 15:01:22 +0100

    suvorov @ macbook-pro

--- suv-bot-deploy.sh
+++ suv-bot-deploy.sh
@@ -5,11 +5,12 @@ source "suv-options.sh"
 
 set -eu
 
+export GIT_REPOSITORY=$GIT_REPOSITORY
 PROJECT_NAME_DELETE=$(pwd)
 
 cd ..
 rm -rf $PROJECT_NAME_DELETE
-git clone "https://github.com/"$GIT_REPOSITORY".git"
+git clone "https://github.com/"$GIT_REPOSITORY
 cd $PROJECT_NAME
 
 ./suv-container.bang
