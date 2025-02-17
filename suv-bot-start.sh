7b45170fcbd8fbee62fc9deeda6535b5d829c749
Alexander Suvorov <alx.suvorov@gmail.com>
2025-02-17 16:06:21 +0100

    suvorov @ macbook-pro

--- suv-bot-start.sh
+++ suv-bot-start.sh
@@ -5,6 +5,11 @@ source "suv-options.sh"
 
 set -eu
 
+echo 
+echo -e ${ANSI_BG_BLUE}"whoami:"${ANSI_OFF}   ${ANSI_BG_PURPLE}$(whoami)${ANSI_OFF}
+echo -e ${ANSI_BG_BLUE}"hostname:"${ANSI_OFF} ${ANSI_BG_PURPLE}$(hostname)${ANSI_OFF}
+echo -e ${ANSI_BG_BLUE}"pwd:"${ANSI_OFF}      ${ANSI_BG_PURPLE}$(pwd)${ANSI_OFF}
+
 # 
 # Запуск бота
 # 
